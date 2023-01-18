8a201806b7885a99c0f438c474e55ea7a11b7da5
Olli Räisä <olli.raisa@trimble.com>
2023-01-18 10:35:01 +0200

    Changed version defs: 3.5.0 230118

--- src/laszip.hpp
+++ src/laszip.hpp
@@ -72,9 +72,9 @@ typedef long long SIGNED_INT64;
 #endif
 
 #define LASZIP_VERSION_MAJOR                3
-#define LASZIP_VERSION_MINOR                4
-#define LASZIP_VERSION_REVISION             3
-#define LASZIP_VERSION_BUILD_DATE      191111
+#define LASZIP_VERSION_MINOR                5
+#define LASZIP_VERSION_REVISION             0
+#define LASZIP_VERSION_BUILD_DATE      230118
 
 #define LASZIP_COMPRESSOR_NONE              0
 #define LASZIP_COMPRESSOR_POINTWISE         1
